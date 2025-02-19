8834358764216e21187c9d7ee98407b08419a327
GMGinIT <gmginit@gmail.com>
2025-02-19 20:05:41 +0300

    db and routes for fav

---
+++ server/src/controllers/Favorite.controller.js
@@ -0,0 +1,43 @@
+const FavoriteService = require("../services/Favorite.service");
+const { formatResponse } = require("../utils/formatResponse");
+
+class FavoriteController {
+  static async addFavorite(req, res) {
+    const { userId, newsId } = req.body;
+    try {
+      const newFav = await FavoriteService.addFav({ userId, newsId });
+      res.status(201).json(formatResponse(201, "success", newFav));
+    } catch ({ message }) {
+      console.error(message);
+      res
+        .status(500)
+        .json(formatResponse(500, "Internal server error", null, message));
+    }
+  }
+
+  static async deleteFromFav(req, res) {
+    const { userId, newsId } = req.body;
+    try {
+      const deletedFav = await FavoriteService.deleteFav({ userId, newsId });
+      res.status(200).json(formatResponse(201, "success", deletedFav));
+    } catch ({ message }) {
+      console.error(message);
+      res
+        .status(500)
+        .json(formatResponse(500, "Internal server error", null, message));
+    }
+  }
+  static async getFavorites(req, res) {
+    const { userId } = req.params;
+    try {
+      const favorites = await FavoriteService.getFav(userId);
+      res.status(200).json(formatResponse(200, "success", favorites));
+    } catch ({ message }) {
+      console.error(message);
+      res
+        .status(500)
+        .json(formatResponse(500, "Internal server error", null, message));
+    }
+  }
+}
+module.exports = FavoriteController;
---
+++ server/src/services/Favorite.service.js
@@ -0,0 +1,24 @@
+const { Favorite } = require("../db/models");
+
+class FavoriteService {
+  static async addFav({ userId, newsId }) {
+    const existingFav = await Favorite.findOne({ where: { userId, newsId } });
+    if (existingFav) {
+      return throw new Error("новость уже добавлена в избранное");
+    }
+    return await Favorite.create({ userId, newsId });
+  }
+  static async deleteFav(userId, newsId) {
+    const favorite = await Favorite.findOne({ where: { userId, newsId } });
+    if (favorite) {
+      await favorite.destroy();
+      return favorite;
+    }
+    return throw new Error("новость не найдена в избранном");
+  }
+  static async getFav(userId) {
+    return await Favorite.findAll({ where: { userId } });
+  }
+}
+
+module.exports = FavoriteService;
