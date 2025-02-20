6bdea788a96a0417b21594603bbdb95e8702965d
Никита Матигулин <nikitamatigulin@MacBook-Pro-Nikita.local>
2025-02-20 20:58:17 +0300

    fix-routes

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const favoriteRoutes = require("../routes/favorite.routes");
 const authRoutes = require("./auth.routes");
+const searchRoutes = require("./search.routes");
 const formatResponse = require("../utils/formatResponse");
 
 router.use("/auth", authRoutes);
 router.use("/favorites", favoriteRoutes);
+router.use("/search", searchRoutes);
 
 router.use("*", (req, res) => {
   res.status(404).json(formatResponse(404, "Not found"));
